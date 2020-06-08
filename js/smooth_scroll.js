2c34c401bcd7ed4fec0b35930b11f23506812d04
dplakhtiy <49567837+dplakhtiy@users.noreply.github.com>
2020-06-08 17:27:05 +0300

    Add files via upload

---
+++ js/smooth_scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  document.querySelectorAll("[data-scrollTo").forEach((item) => {
+    item.addEventListener('click', function () {
+      var elementClick = item.dataset.scrollto;
+      var destination = $(elementClick).offset().top;
+      jQuery("html:not(:animated),body:not(:animated)").animate({
+        scrollTop: destination
+      }, 800);
+      return false;
+    });
+  })
+});
